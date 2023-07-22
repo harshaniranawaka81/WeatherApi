06e5a84e9c488ac29d80a669d78071621cb8e2a9
Harshani Ranawaka <Harshani_Ranawaka@next.co.uk>
2023-07-22 14:57:15 +0100

    Basic structure

---
+++ src/WeatherService.Api/Controllers/StatusController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherService.Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StatusController : ControllerBase
+    {
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [HttpGet]
+        public IActionResult GetStatus() => Ok();
+    }
+}
